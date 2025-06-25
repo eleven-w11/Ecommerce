46b63fb3e07d4c783fea037da55543403d84ca32
eleven <talha.eleven.w11@gmail.com>
2025-06-26 00:06:28 +0500

    server.js

--- backend/server.js
+++ backend/server.js
@@ -74,6 +74,7 @@ app.use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', origin);
     }
 
+    res.setHeader('Access-Control-Allow-Origin', 'https://ecommerce-xdv7.onrender.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
