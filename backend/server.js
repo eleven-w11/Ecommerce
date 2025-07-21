d1aa74f6876d02302a520ff12a07a1b8c708d283
eleven <talha.eleven.w11@gmail.com>
2025-07-21 07:56:57 +0500

    server.js

--- backend/server.js
+++ backend/server.js
@@ -165,10 +165,10 @@ io.on("connection", (socket) => {
 });
 
 // ✅ Serve frontend build folder
-app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../build", "index.html"));
+    res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // ✅ Root test route
