250ba830b857e64c063f0af42fce44d5edec9925
athratalhayesw11-prog <athratalhayesw11@gmail.com>
2025-09-14 22:34:57 +0500

    Refactor BestSelling component styles: clean up CSS by removing commented sections, adjust margins and paddings for improved layout

--- src/files/pages/BestSelling.jsx
+++ src/files/pages/BestSelling.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+ import { useEffect, useState } from "react";
 import axios from "axios";
 import "../styles/BestSelling.css";
 import { Link } from "react-router-dom";
